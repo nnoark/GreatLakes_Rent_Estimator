d78a2e69eea222a3fb0947cc79faaae55ba8e2a4
nnoark <nnoark@gmail.com>
2022-01-21 00:29:19 -0500

    testing

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
  
 @app.route('/result', methods = ['POST'])
 def result():
-    if request == 'POST':
+    if request.method == 'POST':
         to_predict_list = request.form.to_dict()
         to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))

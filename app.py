5bfbc2254fc79f14b61c37cdddb654ea738d3da5
nnoark <nnoark@gmail.com>
2022-01-21 00:00:02 -0500

    testing

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
 
 # prediction function
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 12)
-    loaded_model = pickle.load(open("model.pkl", "rb"))
-    result = loaded_model.predict(to_predict)
+    result = model.predict(to_predict)
     return result[0]
 
 @app.route('/')
